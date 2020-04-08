f25e5f7a81243fa361327dccacdf7c1acb8a8034
Hin Wong <hinsolo@Hins-MacBook-Pro.local>
2020-04-08 16:12:55 -0600

    Outlets and actions complete

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IB Outlet
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    //MARK: - IB Actions
+    
+    @IBAction func buttons(_ sender: Any) {
+    }
+    
+    @IBAction func restartButton(_ sender: Any) {
+    }
+    
 
 }
 
