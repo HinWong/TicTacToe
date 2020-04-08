7deb00c4ecf4a31922d70ce9e9b6c7e4b231bbae
Hin Wong <hinsolo@Hins-MacBook-Pro.local>
2020-04-08 17:41:14 -0600

    button connections not working

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    var currentPlayer = 1 // 1 is Joe, 2 is Carol
+    
 
     //MARK: - IB Outlet
-    
     @IBOutlet weak var messageLabel: UILabel!
     
     override func viewDidLoad() {
@@ -19,8 +22,14 @@ class ViewController: UIViewController {
     }
     
     //MARK: - IB Actions
-    
-    @IBAction func buttons(_ sender: Any) {
+    @IBAction func buttons(_ sender: UIButton) {
+        if currentPlayer == 1 {
+            sender.setImage(UIImage(named: "JoeExotic"), for: [])
+            currentPlayer = 2
+        } else {
+            sender.setImage(UIImage(named: "CaroleBaskin"), for: [])
+            currentPlayer = 1
+        }
     }
     
     @IBAction func restartButton(_ sender: Any) {
